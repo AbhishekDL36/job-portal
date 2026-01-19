35bd0501bb62147c725d47c14933f8d4939243a8
Abhishek <abhishekofficials12@gmail.com>
2026-01-19 10:39:53 +0530

    --

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || "https://job-backend-owxg.onrender.com/api"
+const API_URL =  "https://job-backend-owxg.onrender.com/api"
 
 console.log('🌐 API URL configured:', API_URL);
 
